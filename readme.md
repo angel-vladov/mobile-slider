7490f79b00bd0e385cf2b08ad07023cf0eee1c9a
Angel Vladov <angel.vladov@d-consult.net>
2016-02-05 14:35:23 +0200

    documentation update

--- readme.md
+++ readme.md
@@ -11,7 +11,7 @@ Download the [production version][min] or the [development version][max]. Downlo
 [max]: https://raw.githubusercontent.com/angel-vladov/mobile-slider/master/dist/jquery.mobile-slider.js
 [styles]: https://raw.githubusercontent.com/angel-vladov/mobile-slider/master/dist/jquery.mobile-slider.css
 
-In your web page:
+In your web page import the javascript file and the stylesheet that come with the plugin:
 
 ```html
 <link href="dist/jquery.mobile-slider.css" rel="stylesheet" type="text/css">
@@ -43,23 +43,68 @@ _Using attributes:_
 ```html
 <div class="mobile-slider" data-item-width="260" data-slider-when="1024">
 ```
-Prefixing attributes with _"data-"_ is optional. All the options can be passed as attributes.
+Prefixing attributes with _"data-"_ is optional. 
+All the options can be passed as attributes.
 
 Supported options:
 #### sliderWhen
 
-Type: `number`
-Default: `1024`
+Type: `number`  
+Default: `1024`  
 
 Min window width needed for the mobile slider to apply and change the layout of the element it's applied to. Nothing will happen if the browser window is wider than `sliderWhen` width _(1024px with default value)_.
 
 #### itemWidth
 
-Type: `number` or `string`
+Type: `number` or `string`  
+Example: `260`, `"260px"`, `"4.2em"`, `"80%"`  
 
 If you need a specific width to be set on each list item when the mobile slider is active you can use this option. Numeric values are in pixels. If you need a value in em or percents pass a string.
 
-Example: `260px`, `4.2em`, `80%`
+#### container
+
+Type: `jQuery`, `DOM Element`, a `string` selector    
+Default: `window`  
+
+The plugin will use this selector/element width and check it against `itemWidth` in order to determine when the slider should be active.
+
+### Usage
+
+* The plugin will be attached automatically on all elements that have `.mobile-slider` class. You can use attributes to set options values.
+Example:  
+
+```html
+<div class="mobile-slider" data-item-width="220">
+    <ul>
+        <li>Slide 1</li>
+        <li>Slide 2</li>
+        <li>Slide 3</li>
+        <li>Slide 4</li>
+        <li>Slide 5</li>
+    </ul>
+</div>
+```
+
+* You can also attach the plugin manually using javascript. `.mobile-slider` will be attached for you by the plugin.
+Example:  
+
+```html
+<div id="my-slider">
+    <ul>
+        <li>Slide 1</li>
+        <li>Slide 2</li>
+        <li>Slide 3</li>
+        <li>Slide 4</li>
+        <li>Slide 5</li>
+    </ul>
+</div>
+
+<script type="application/javascript">
+    $('#my-slider').mobileSlider({
+    	itemWidth: 220
+    });
+</script>
+```
 
 
 ## License
--- src/mobile-slider.js
+++ src/mobile-slider.js
@@ -7,6 +7,30 @@
 (function ($) {
 	'use strict';
 
+	function readAttributeOptions($element, opts) {
+		var itemWidth = $element.attr('data-item-width') || $element.attr('item-width');
+		var sliderWhen = $element.attr('data-slider-when') || $element.attr('slider-when');
+
+		if (itemWidth) {
+			opts.itemWidth = itemWidth;
+		}
+
+		if (sliderWhen) {
+			opts.sliderWhen = sliderWhen;
+		}
+	}
+
+	function normalizeOptions(opts) {
+		if (typeof opts.itemWidth === 'string') {
+			var lastChar = opts.itemWidth.charAt(opts.itemWidth.length  - 1);
+
+			if (!isNaN(parseInt(lastChar))) {
+				// Whole string is a number. Append px.
+				opts.itemWidth += 'px';
+			}
+		}
+	}
+
 	$.fn.mobileSlider = function(options) {
 		var opts = $.extend({}, $.fn.mobileSlider.defaults, options);
 
@@ -19,26 +43,13 @@
 			var initialized = false;
 			var edgeOffset = 0;
 
-			function readAttributeOptions() {
-				var itemWidth = $container.attr('data-item-width') || $container.attr('item-width');
-				var sliderWhen = $container.attr('data-slider-when') || $container.attr('slider-when');
-
-				if (itemWidth) {
-					opts.itemWidth = itemWidth;
-				}
-
-				if (sliderWhen) {
-					opts.sliderWhen = sliderWhen;
-				}
-			}
-
 			function initializeNav() {
 				var navMarkup = '<div class="slider-dots-list">';
 
 				$nodes.each(function(index) {
 					var $node = $(this);
 
-					$node.data('c-id', index);
+					$node.data('slide-id', index);
 
 					navMarkup += '<div class="slider-dot" data-node="' + index + '"></div>';
 				});
@@ -76,7 +87,7 @@
 				if (!$lastActiveNode || $lastActiveNode[0] !== $lastMatched[0]) {
 					$lastActiveNode = $lastMatched;
 
-					var nodeIndex = $lastActiveNode.data('c-id');
+					var nodeIndex = $lastActiveNode.data('slide-id');
 					var nodeSelector = '[data-node="' + nodeIndex + '"]';
 
 					$nodes.removeClass('active');
@@ -105,7 +116,7 @@
 				$nav.on('click', '.goto-prev', function(e) {
 					e.preventDefault();
 
-					var currentIndex = $lastActiveNode.data('c-id');
+					var currentIndex = $lastActiveNode.data('slide-id');
 					var prevIndex = currentIndex - 1;
 
 					if (prevIndex < 0) {
@@ -120,7 +131,7 @@
 				$nav.on('click', '.goto-next', function(e) {
 					e.preventDefault();
 
-					var currentIndex = $lastActiveNode.data('c-id');
+					var currentIndex = $lastActiveNode.data('slide-id');
 					var nextIndex = (currentIndex + 1) % $nodes.length;
 
 					scrollToNode($nodes[nextIndex]);
@@ -130,7 +141,7 @@
 			}
 
 			function handleOnResize() {
-				var screenWidth = $(window).width();
+				var screenWidth = $(opts.container).width();
 				var sliderIsActive = $container.hasClass('slider-active');
 
 				if (screenWidth <= opts.sliderWhen) {
@@ -172,10 +183,8 @@
 				$viewPane.addClass('slider-content');
 
 				if ($nodes.length > 0) {
-					var itemWidth = $container.attr('data-item-width') || $container.attr('item-width');
-
-					if (itemWidth) {
-						$nodes.width(itemWidth);
+					if (opts.itemWidth) {
+						$nodes.width(opts.itemWidth);
 					}
 
 					initializeNav();
@@ -191,7 +200,12 @@
 				}
 			}
 
-			readAttributeOptions();
+			if (!$container.hasClass('.mobile-slider')) {
+				$container.addClass('mobile-slider');
+			}
+
+			readAttributeOptions($container, opts);
+			normalizeOptions(opts);
 
 			$(window).on('resize orientationchange', handleOnResize);
 			handleOnResize();
@@ -200,7 +214,8 @@
 
 	$.fn.mobileSlider.defaults = {
 		itemWidth: null,
-		sliderWhen: 1024
+		sliderWhen: 1024,
+		container: window
 	};
 
 	$(document).ready(function() {
